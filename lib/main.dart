d663bbe298e0eba22804829c8027ab471f224633
rao-suraj <surajrao250@gmail.com>
2024-12-29 20:18:02 +0530

    final_commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
+
   WidgetsFlutterBinding.ensureInitialized();
+  
   await Firebase.initializeApp();
   configureDependencies();
   await HiveInitializer.initialize();
